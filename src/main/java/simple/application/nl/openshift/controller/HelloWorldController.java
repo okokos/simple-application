4b27b4e383c6a186868086d0c24780c0de3d3695
skost <s.kostany@owinsp.nl>
2022-01-01 17:57:13 +0100

    buildspec voor S2I

--- src/main/java/simple/application/nl/openshift/controller/HelloWorldController.java
+++ src/main/java/simple/application/nl/openshift/controller/HelloWorldController.java
@@ -3,10 +3,19 @@ package simple.application.nl.openshift.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+
 @RestController
 public class HelloWorldController {
     @RequestMapping("/")
     public String hello() {
-        return "Hello javaTpoint";
+        String host = "";
+        try {
+            host = InetAddress.getLocalHost().getHostName();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "Hello javaTpoint" +"I am running on server "+host+" Version 1.0 \n";
     }
 }
